8c52db79b688bc99716b2c798fd6e0a594444d4c
Travis Lavery <travisalavery@gmail.com>
2017-11-21 02:17:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObj
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
