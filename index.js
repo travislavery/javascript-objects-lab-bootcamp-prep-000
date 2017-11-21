85742839c178b2df73c79c1f3bf3df9dda7e077d
Travis Lavery <travisalavery@gmail.com>
2017-11-21 02:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObj = Object.assign({}, object, {key : value})
+  var newObj = Object.assign({}, object)
+  newObj[key] = value
   return newObj
 }
 
